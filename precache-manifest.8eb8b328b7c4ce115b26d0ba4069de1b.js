f0d7115cc5c5ef43d8bdbf12617e9acfac2b9634
Marshall Rogers <rogers.marshall@gmail.com>
2020-10-14 21:02:09 -0700

    Add files via upload

---
+++ precache-manifest.8eb8b328b7c4ce115b26d0ba4069de1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e88b76b5497f54bd2ed6a42d39651b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e06c130abc847174e97",
+    "url": "/static/css/main.d2acad4d.chunk.css"
+  },
+  {
+    "revision": "b1c92c2a95b0d395410c",
+    "url": "/static/js/2.cd45c90c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd45c90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e06c130abc847174e97",
+    "url": "/static/js/main.d7e544bd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
